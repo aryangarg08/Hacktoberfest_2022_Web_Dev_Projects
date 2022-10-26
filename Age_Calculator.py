a9090c9ec4c077758e89302732f20c8031b35370
musaibbatghar <111275468+musaibbatghar@users.noreply.github.com>
2022-10-26 12:12:43 +0530

    Age Calculating Python Program

---
+++ Age_Calculator.py
@@ -0,0 +1,16 @@
+# Python3 code to calculate age in years
+
+from datetime import date
+
+
+def calculateAge(birthDate):
+    today = date.today()
+    age = today.year - birthDate.year
+    ((today.month, today.day) <
+     (birthDate.month, birthDate.day))
+
+
+    return age
+
+
+print(calculateAge(date(1997, 2, 3)), "years")
